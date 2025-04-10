6a0d5454bf2c6b25662e3e7f7020ce6b0ad2a93c
root <root@LAPTOP-B3AVV9SO.>
2025-04-10 12:32:26 -0400

    refactoring

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d "$1" -silent | tee >(while IFS= read -r sub; do echo "$sub,$(host "$sub" | awk '/has address/ {print $4; exit}')"; done > "$1.txt")
+subfinder -d "$1" -silent -nW -o "$1.txt" -oI
