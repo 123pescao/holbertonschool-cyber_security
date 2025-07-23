42b9ff9aa29b376f306b3254cac097fcffa29e7b
123pescao <joel.caban2017@gmail.com>
2025-07-23 14:21:48 -0400

    Push task

--- web_application_security/0x0b_web_application_fast_incident_response/2-count_attack.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/2-count_attack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-attacker=$(awk '{print $1}' $1 | sort | uniq -c | sort -nr | head -1 | awk '{print $2}')
-grep "^$attacker " $1 | wc -l
+attacker_ip=$(awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
+grep -c "^$attacker_ip" logs.txt
