0e64aeb1ccea4d791d7be21a7342c3849522903d
123pescao <joel.caban2017@gmail.com>
2025-08-31 17:27:44 -0400

    Fixing task

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -2,18 +2,12 @@
 
 
 def print_arguments
-  if ARGV.length == 0
-    puts 'No arguments provided.'
-    return
-  end
-
-  index = 1
-  ARGV.each do |arg|
-    puts index.to_s + '. ' + arg
-    index += 1
-  end
-end
-
-if __FILE__ == $0
-  print_arguments
+    if ARGV.empty?
+        puts "No arguments provided."
+    else
+        puts "Arguments:"
+        ARGV.each_with_index do |arg, index|
+            puts "#{index + 1}. #{arg}"
+        end
+    end
 end
