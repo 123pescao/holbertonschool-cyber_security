1b37ecc86076b73f31d3fcf361def367e9b834eb
123pescao <joel.caban2017@gmail.com>
2025-08-31 17:28:40 -0400

    Fixing task

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -1,41 +1,30 @@
 #!/usr/bin/env ruby
 
 require 'net/http'
-require 'uri'
 require 'json'
+require 'uri'
 
-def post_request(url, body_params)
-  uri = URI.parse(url)
-  http = Net::HTTP.new(uri.host, uri.port)
-  if uri.scheme == 'https'
-    http.use_ssl = true
-  end
+def post_request(url, body_params = {})
+  uri = URI(url)
 
-  request = Net::HTTP::Post.new(uri.request_uri)
-  request['Content-Type'] = 'application/json'
-  request.body = JSON.generate(body_params)
+  # Create HTTP request with headers
+  headers = { 'Content-Type' => 'application/json' }
+  response = Net::HTTP.post(uri, body_params.to_json, headers)
 
-  response = http.request(request)
+  # Print response status
+  puts "Response status: #{response.code} #{response.message}"
 
-  status_line = response.code.to_s + ' ' + response.message.to_s
-  puts 'Response status: ' + status_line
-  puts 'Response body:'
-
-  body = response.body
+  # Try parsing as JSON
   begin
-    parsed = JSON.parse(body)
-    puts JSON.pretty_generate(parsed)
+    json_body = JSON.parse(response.body)
+    puts "Response body:"
+    if json_body.empty?
+        puts JSON.generate(json_body)
+    else
+        puts JSON.pretty_generate(json_body)
+    end
   rescue JSON::ParserError
-    puts body
-  end
-end
-
-if __FILE__ == $0
-  if ARGV.length >= 1
-    url = ARGV[0]
-    params = {}
-    post_request(url, params)
-  else
-    puts 'Usage: 8-post.rb URL'
+    puts "Response body:"
+    puts response.body
   end
 end
